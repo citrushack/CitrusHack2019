d59f34084cf9ba27fa75dc9bf9495ab4634dd468
jkim393 <jkim393@ucr.edu>
2019-02-27 16:56:16 -0800

    took down apply page

--- citrus/signup/urls.py
+++ citrus/signup/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 # app_name = 'signup'
 urlpatterns = [
-    path('apply/', views.apply, name='apply'),
+    #path('apply/', views.apply, name='apply'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='signup/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='signup/logged_out.html'), name='logout'),

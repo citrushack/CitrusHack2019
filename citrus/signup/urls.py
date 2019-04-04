7592e9e120d98b816e229807dd2810a6ec9e0151
Gabriel Gomez <ggome009@ucr.edu>
2019-04-04 11:29:38 -0700

    removed application/edit profile links & disabled apply and edit URLS

--- citrus/signup/urls.py
+++ citrus/signup/urls.py
@@ -5,11 +5,11 @@ from django.conf import settings
 
 # app_name = 'signup'
 urlpatterns = [
-    path('apply/', views.apply, name='apply'),
+    # path('apply/', views.apply, name='apply'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='signup/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='signup/logged_out.html'), name='logout'),
-    path('edit/', views.update_profile, name='edit'),
+    # path('edit/', views.update_profile, name='edit'),
     
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),

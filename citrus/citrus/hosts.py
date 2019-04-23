ce0f5fe072b7312a15b604fdf8c7b1dbfb1d7133
jkim393 <jkim393@ucr.edu>
2019-04-23 11:08:51 -0700

    subdomain files

---
+++ citrus/citrus/hosts.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django_hosts import patterns, host
+
+host_patterns = patterns(
+    '',
+    #host(r'www', 'subdomains_tutorial.frontend_urls', name='www'),
+    #host(r'admin', settings.ROOT_URLCONF, name='admin'),
+    #host(r'api', 'subdomains_tutorial.api_urls', name='api'),
+    host(r'live', 'citrus.live_urls', name='live'),
+)
